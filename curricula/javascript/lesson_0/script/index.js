d8e5e67e6dd846e6187d18a1707e89109d6ae3cb
16sheep <marjutubli@gmail.com>
2019-10-30 11:59:28 +0000

    Add js to lesson 0

---
+++ curricula/javascript/lesson_0/script/index.js
@@ -0,0 +1,82 @@
+
+// console.log(varHelloWorld)
+// var varHelloWorld = "Hello World"
+
+
+// console.log(letHelloWorld)
+// let letHelloWorld = "Hello World"
+
+
+// var functionScoped = "hi"
+// {
+// 	var functionScoped = "bye"
+// }
+
+// let blockScoped = "hi"
+// {
+// 	let blockScoped = "bye"
+// }
+
+
+// Conditional statement
+// const meaningOfLife = 42
+
+// if (meaningOfLife === 42) {
+//     console.log("Thanks for all the fish")
+//   } else if (meaningOfLife > 40 && meaningOfLife < 44) {
+//     console.log("Not quite the answer I was expecting")
+//   } else {
+//     console.log("That's too far off")
+//   }
+
+
+
+// The while loop loops through a block of code as long as a specified condition is true.
+// let text = ""
+// let loopNum = 0
+// while (text.length < 50) {
+//     loopNum++
+// 	   console.log(text.length)
+//     text += ` The number is ${loopNum}`
+//   }
+
+
+
+/* Given you have string "0123456789" please use a while loop to iterate over it.
+The goal is to have with 2 variables called numOdd and numEven with values of how many items in that
+string times 3 are even and how many odd, these values should be numbers. 
+at each iteration we should log out to the console a string which says
+*/
+
+
+
+
+
+
+
+
+
+
+/* 
+Consider this array 
+[["oops",7,"oops",0],
+[-10,0,0,0],
+["1","7","6","0"],
+[78,9,7.4,3,1]]
+
+As you notice there are several arrays within this one. 
+Please find a way to produce a new array from this 
+which should be made up of arrays. Those arrays should
+hold 2 values: 
+The smallest number of the nested array, 
+and the biggest number of the nested array
+
+If the array doesnt hold any numeric values it should insert a string
+to the new array  "No numbers found"
+
+The resulting array should look like this:
+[[0, 7], [-10, 0], "No numbers found",[1, 78]]
+
+Keep in mind the nested arrays are not the same length and 
+not all the values are of same type.
+*/
